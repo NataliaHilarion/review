3f636795ccd2a933fd4e28f7fdac7b994501dfe7
Natalia Hilarion <natalia.hilarion@outlook.com>
2023-05-10 15:56:42 -0500

    functions completas

--- AppJs/app.js
+++ AppJs/app.js
@@ -1,3 +1,51 @@
 import { reviews } from "./users.js";
 
-console.log(reviews);
+const $img = document.getElementById("person-img")
+const $author = document.getElementById("author");
+const $job = document.getElementById("job")
+const $info = document.getElementById("info");
+
+const $btnPrev = document.querySelector(".prev-btn")
+const $btnNext = document.querySelector(".next-btn")
+const $btnRandom = document.querySelector(".random-btn");
+
+let currentItem = 1
+
+window.addEventListener("DOMContentLoaded", function(){
+  return showPerson()
+})
+
+function showPerson(){
+   return personInformation(currentItem)
+}
+
+function personInformation(person){
+  const item = reviews[person];
+  $img.src = item.img;
+  $author.textContent = item.name;
+  $job.textContent = item.job;
+  $info.textContent = item.text;
+}
+
+$btnPrev.addEventListener("click", function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson()
+});
+
+$btnNext.addEventListener("click", function () {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson()
+});
+
+$btnRandom.addEventListener("click", function () {
+  const random = Math.floor(Math.random() * reviews.length)
+  currentItem = random
+  showPerson()
+})
+
--- AppJs/users.js
+++ AppJs/users.js
@@ -4,28 +4,28 @@ export const reviews = [
     name: "Ana smith",
     job: "web developer",
     img: "./resources/ana.jpg",
-    text: "Como desarrollador web, me complace decir que he tenido una experiencia increíble visitando esta página web. Su diseño intuitivo y fácil de navegar, combinado con su contenido interesante y bien estructurado, me mantuvo enganchado durante horas. Además, la velocidad de carga rápida y la capacidad de respuesta de la página en diferentes dispositivos es impresionante. En general, esta es una excelente página que recomendaría a cualquiera que esté buscando información en línea de alta calidad.",
+    text: "Como desarrollador web, me complace decir que he tenido una experiencia increíble visitando esta página web. Su diseño intuitivo y fácil de navegar, combinado con su contenido interesante y bien estructurado, me mantuvo enganchado durante horas. Además, la velocidad de carga rápidaen diferentes dispositivos es impresionante..",
   },
   {
     id: 2,
     name: "Luisa johnson",
     job: "web designer",
     img: "./resources/luisa.jpg",
-    text: "Como diseñador web, debo decir que estoy impresionado con esta página. El diseño es hermoso y elegante, con una excelente elección de colores y una tipografía cuidadosamente seleccionada. Me encanta cómo la página utiliza una combinación de imágenes y texto para transmitir información de manera efectiva y atractiva. Además, la navegación es intuitiva y fácil de usar, lo que hace que la experiencia del usuario sea aún mejor. En general, esta es una página web de alta calidad que demuestra una gran atención al detalle y un enfoque en el diseño centrado en el usuario",
+    text: "Como diseñador web, debo decir que estoy impresionado con esta página. El diseño es hermoso y elegante, con una excelente elección de colores y una tipografía cuidadosamente seleccionada. Me encanta cómo la página utiliza una combinación de imágenes y texto para transmitir información de manera efectiva y atractiva.",
   },
   {
     id: 3,
     name: "Pedro jones",
     job: "intern",
     img: "./resources/pedro.jpg",
-    text: "Como intern, me gustaría expresar mi entusiasmo por esta página web. Aunque mi experiencia es limitada, puedo decir que la página es fácil de usar y muy informativa. Me encanta cómo la página presenta la información de manera clara y concisa, y cómo la navegación es sencilla e intuitiva. Además, me sorprendió gratamente lo rápido que se cargó la página, lo que hizo que mi experiencia fuera aún mejor. En general, esta es una página web impresionante que se esfuerza por brindar una experiencia del usuario excelente y efectiva.",
+    text: "Aunque mi experiencia es limitada, puedo decir que la página es fácil de usar y muy informativa. Me encanta cómo la página presenta la información de manera clara y concisa, y cómo la navegación es sencilla e intuitiva. Además, me sorprendió gratamente lo rápido que se cargó la página, lo que hizo que mi experiencia fuera aún mejor.",
   },
   {
     id: 4,
     name: "Rodrigo anderson",
     job: "QA",
     img: "./resources/rodrigo.jpg",
-    text: "Como QA, estoy impresionado por la calidad y la estabilidad de esta página web. La página funcionó sin problemas en todas las pruebas que realicé, lo que indica un alto nivel de calidad y una excelente capacidad de rendimiento. Además, aprecio cómo la página está diseñada de manera que hace que la interacción del usuario sea fácil y libre de errores. En general, esta es una página web que está bien diseñada y bien construida, lo que la hace muy confiable y fácil de usar para los usuarios finales.",
+    text: "Como QA, estoy impresionado por la calidad y la estabilidad de esta página web. La página funcionó sin problemas en todas las pruebas que realicé, lo que indica un alto nivel de calidad y una excelente capacidad de rendimiento. Además, aprecio cómo la página está diseñada de manera que hace que la interacción del usuario sea fácil y libre de errores.",
   },
 ];
 
